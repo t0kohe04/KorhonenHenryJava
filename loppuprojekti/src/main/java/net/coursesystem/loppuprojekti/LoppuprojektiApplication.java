0e4c7f44a24f5e00ed45a4f9c3bd34a135256e70
Henry Korhonen <t0kohe04@students.oamk.fi>
2021-11-10 12:45:51 +0200

    re-initialized spring boot

---
+++ loppuprojekti/src/main/java/net/coursesystem/loppuprojekti/LoppuprojektiApplication.java
@@ -0,0 +1,13 @@
+package net.coursesystem.loppuprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoppuprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoppuprojektiApplication.class, args);
+	}
+
+}
